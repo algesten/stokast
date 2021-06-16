2da6b564b6a2424da40ecbedd80de1c88ccf057c
Martin Algesten <martin@algesten.se>
2021-06-16 16:21:33 +0200

    teensy4.0

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,21 @@
 #![no_std]
 #![no_main]
 
-use core::panic::PanicInfo;
-use cortex_m_rt::{entry, exception, ExceptionFrame};
-use stm32h7::stm32h747cm7;
+use teensy4_bsp as bsp;
+use teensy4_panic as _;
 
-#[entry]
+const LED_PERIOD_MS: u32 = 200;
+
+#[cortex_m_rt::entry]
 fn main() -> ! {
-    let peripherals = stm32h747cm7::Peripherals::take().unwrap();
-    let gpioa = &peripherals.GPIOA;
+    let p = bsp::Peripherals::take().unwrap();
+    let mut systick = bsp::SysTick::new(cortex_m::Peripherals::take().unwrap().SYST);
+
+    let pins = bsp::t40::into_pins(p.iomuxc);
+    let mut led = bsp::configure_led(pins.p13);
 
     loop {
-        gpioa.odr.modify(|_, w| w.odr0().set_bit());
-    }
-}
-
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {
-        //
-    }
-}
-
-// More here: https://docs.rs/cortex-m-rt/0.6.14/cortex_m_rt/attr.exception.html
-#[exception]
-fn HardFault(_ef: &ExceptionFrame) -> ! {
-    loop {
-        //
+        led.toggle();
+        systick.delay(LED_PERIOD_MS);
     }
 }
